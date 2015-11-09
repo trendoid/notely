01beebfbdb509febd1a19c4396f3683ad7eadcad
Aaron Stanley King <trendoid@outlook.com>
2015-11-09 16:55:44 -0500

    form

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -10,7 +10,7 @@
 
 			.state('notes', {
 				url: '/notes',
-				template: '<h1>Notely</h1><p>{{ message }}</p><div ui-view></div>',
+				templateUrl: '/notes/notes.html',
 				controller: NotesController
 			})
 			.state('notes.form', {
@@ -20,8 +20,8 @@
 			});
 	}
 	
-	NotesController['$inject'] = ['$scope'];
-	function NotesController($scope){
-		$scope.message = "I <3 Angular.";
+	NotesController['$inject'] = ['$scope', '$state'];
+	function NotesController($scope, $state){
+		$state.go('notes.form');
 	}
 })();
