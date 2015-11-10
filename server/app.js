468a7c0b0bdfbec17dd96cb2a28c6b6bbcc062d5
Aaron Stanley King <trendoid@outlook.com>
2015-11-10 14:59:47 -0500

    working mongodb get

--- server/app.js
+++ server/app.js
@@ -1,27 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.use(function(req, res, next){
+var db = require('mongoose');
+
+db.connect('mongodb://mongo:mongo123@ds039880.mongolab.com:39880/notelydb')
+
+var NoteSchema = db.Schema({
+	title: String,
+	body_Html: String,
+	body_text: String,
+	updated_at: { type: Date, default: Date.now }
+});
+
+var Note = db.model('Note', NoteSchema);
+
+app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
 
-app.get('/notes', function(req, res){
-	res.json(
-		[
-			{
-				title: 'hardcoded title',
-				body_html: 'this is the body'
-			},
-			{
-				title: 'title 2',
-				body_html: 'crap crap crap'
-			}
-		]
-	);
+// List all notes
+app.get('/notes', function (req, res) {
+	Note.find().then(function(notes){
+		res.json(notes);
+	});
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
 	console.log('Listening on http://localhost:3000');
 });
