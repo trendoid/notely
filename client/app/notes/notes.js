332004386e2644f24c22b8fb174c7d072469d42a
Aaron Stanley King <trendoid@outlook.com>
2015-11-11 10:18:49 -0500

    note by id UI hooked up, sort of

--- client/app/notes/notes.js
+++ client/app/notes/notes.js
@@ -10,31 +10,34 @@
 
 			.state('notes', {
 				url: '/notes',
+				resolve: {
+					notesLoaded: ['NotesService', function (NotesService) {
+						return NotesService.fetch();
+					}]
+				},
 				templateUrl: '/notes/notes.html',
 				controller: NotesController
 			})
 			.state('notes.form', {
 				url: '/:noteId',
-				templateUrl: '/notes/notes-form.html'
+				templateUrl: '/notes/notes-form.html',
+				controller: NotesFormController
 			});
 	}
 
 	NotesController.$inject = ['$state', '$scope', 'NotesService'];
 	function NotesController($state, $scope, NotesService) {
-		$scope.note = {};
-
-		$scope.save = function () {		
-			NotesService.save($scope.note);
-
-		};
-
-		NotesService.fetch().then(function () {
-			$scope.notes = NotesService.get();
-			
-			//Test NotesService#findById
-			var note = NotesService.findById($scope.notes[0]._id);
-			console.log(note.title);
-		});
-
+		$scope.notes = NotesService.get();
 	}
+
+	NotesFormController.$inject = ['$state', '$scope', 'NotesService'];
+	function NotesFormController($state, $scope, NotesService) {
+
+		$scope.note = NotesService.findById($state.params.noteId);
+
+		$scope.save = function () {
+			NotesService.save($scope.note);
+		};
+	}
+
 })();
